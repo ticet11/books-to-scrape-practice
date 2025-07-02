f95b7d62cc57a497e178271a9e71e724b4d030d5
Brian Kozub <ticet11@gmail.com>
2025-07-02 15:44:58 -0400

    Successfully pulling books from all pages, links are now incorrectly generated.

--- scrape.js
+++ scrape.js
@@ -6,39 +6,46 @@ const scrape = async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 
-	const url = "https://books.toscrape.com/";
+	const allBooks = [];
+	const startingPage = 1;
+	const maxPages = 5;
 
-	await page.goto(url);
+	for (let i = startingPage; i < maxPages; i++) {
+		const url = `https://books.toscrape.com/catalogue/page-${i}.html`;
 
-	const books = await page.evaluate(() => {
-		const bookElements = document.querySelectorAll(".product_pod");
-		const bookArray = Array.from(bookElements).map((book) => {
-			const title = book.querySelector("h3 a").getAttribute("title");
-			const price = book.querySelector(".price_color").textContent;
-			const availability =
-				book.querySelector(".availability").textContent.trim() || "Not available";
-			const numbersObj = {
-				Zero: 0,
-				One: 1,
-				Two: 2,
-				Three: 3,
-				Four: 4,
-				Five: 5,
-			};
-			const text_rating = book.querySelector("p.star-rating").className.split(" ")[1];
-			const rating = numbersObj[text_rating] || 0;
-			const link = `${location.href}${book.querySelector("h3 a").getAttribute("href")}`;
-			return {
-				title,
-				price,
-				availability,
-				rating,
-				link,
-			};
+		await page.goto(url);
+
+		const books = await page.evaluate(() => {
+			const bookElements = document.querySelectorAll(".product_pod");
+			const bookArray = Array.from(bookElements).map((book) => {
+				const title = book.querySelector("h3 a").getAttribute("title");
+				const price = book.querySelector(".price_color").textContent;
+				const availability =
+					book.querySelector(".availability").textContent.trim() || "Not available";
+				const numbersObj = {
+					Zero: 0,
+					One: 1,
+					Two: 2,
+					Three: 3,
+					Four: 4,
+					Five: 5,
+				};
+				const text_rating = book.querySelector("p.star-rating").className.split(" ")[1];
+				const rating = numbersObj[text_rating] || 0;
+				const link = `${location.href}${book.querySelector("h3 a").getAttribute("href")}`;
+				return {
+					title,
+					price,
+					availability,
+					rating,
+					link,
+				};
+			});
+			return bookArray;
 		});
-		return bookArray;
-	});
-	fs.writeFileSync("books.json", JSON.stringify(books, null, 2));
+        allBooks.push(...books);
+	}
+	fs.writeFileSync("books.json", JSON.stringify(allBooks, null, 2));
 
 	await browser.close();
 };
